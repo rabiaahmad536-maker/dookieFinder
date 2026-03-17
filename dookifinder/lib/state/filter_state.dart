64cafb3b9eb8c35f87d1105d0ebdf9eea191083f
Masha Berzoi <Masha.berzoi@gmail.com>
2026-03-17 15:32:11 -0400

    Front end of menu bar (not connected to any data)

---
+++ dookifinder/lib/state/filter_state.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class FilterState extends ChangeNotifier {
+  //default values for filters, all false
+  bool accessibility = false;
+  bool genderNeutral = false;
+  bool singleStall = false;
+  double minRating = 0;
+
+  //
+  void update({
+    bool? accessibility,
+    bool? genderNeutral,
+    bool? singleStall,
+    double? minRating,
+  }) {
+    if (accessibility != null) this.accessibility = accessibility;
+    if (genderNeutral != null) this.genderNeutral = genderNeutral;
+    if (singleStall != null) this.singleStall = singleStall;
+    if (minRating != null) this.minRating = minRating;
+    notifyListeners(); // triggers map to re-filter markers
+  }
+}
